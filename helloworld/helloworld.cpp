11103023d91b1df38d94c8ecfd4807117285a751
denv3rr <denver.clark@g.austincc.edu>
2024-08-28 14:47:18 -0500

    testing remote with helloworld c++

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void helloworld()
+{
+    std::cout << "Hello World\n\n";
+}
---
+++ helloworld/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "helloworld.h"
+
+int main()
+{
+    helloworld();
+    return 0;
+}
